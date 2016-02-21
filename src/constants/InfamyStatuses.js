25c48c021a93e57b3e6bf780e277a0e0b1db2778
Shisha <shisha225@gmail.com>
2016-02-21 19:00:10 +0800

    新增 InfamyTreeHandler 相關功能

---
+++ src/constants/InfamyStatuses.js
@@ -0,0 +1,4 @@
+export const STATUS_LOCKED = 'STATUS_LOCKED';
+export const STATUS_UNLOCKED = 'STATUS_UNLOCKED';
+export const STATUS_OWNED = 'STATUS_OWNED';
+export const STATUS_DISABLE = 'STATUS_DISABLE';
