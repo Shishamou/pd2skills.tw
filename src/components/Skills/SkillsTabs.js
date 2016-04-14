5047e65382d49c531b90d7bcf98d39029f35419e
Shisha <shisha225@gmail.com>
2016-04-14 23:42:50 +0800

    更新 SkillsTabs 為新版技能

--- src/components/Skills/SkillsTabs.js
+++ src/components/Skills/SkillsTabs.js
@@ -3,23 +3,24 @@ import React, { Component, PropTypes } from 'react';
 class SkillsTabs extends Component
 {
 	render() {
-		const { locale, trees, activedTree, onClick } = this.props;
-
 		return (
 			<div className="section-tabs">
 				<div className="section-tabs-main">
-					<div className="section-tabs-contain">
-						{trees.map((tree, index) =>
-							<li key={index}
-								className={(index === activedTree)? 'section-tab actived' : 'section-tab'}
-								onClick={(e) => {onClick(index)}}
-							>
-								<span>{locale(`st_menu_${tree.name}`)}</span>
-								<span>({tree.spendPoints})</span>
-							</li>
-						)}
-					</div>
+					<div className="section-tabs-contain">{this.renderTabs()}</div>
 				</div>
+			</div>
+		);
+	}
+
+	renderTabs() {
+		const { locale, masterTrees, activedTree, onClick } = this.props;
+
+		return masterTrees.map((tree, index) =>
+			<div key={index}
+				className={(index === activedTree)? 'section-tab actived' : 'section-tab'}
+				onClick={(e) => {onClick(index)}}
+			>
+				<span>{locale(`st_menu_${tree.name}`)}</span>
 			</div>
 		);
 	}
@@ -27,7 +28,7 @@ class SkillsTabs extends Component
 
 SkillsTabs.propTypes = {
 	locale: PropTypes.func.isRequired,
-	trees: PropTypes.array.isRequired,
+	masterTrees: PropTypes.array.isRequired,
 	activedTree: PropTypes.number,
 	onClick: PropTypes.func.isRequired,
 };
