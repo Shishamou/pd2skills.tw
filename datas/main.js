fc1b0c641c52ea7d4a73f3c3537fb96df64807f2
Shisha <shisha225@gmail.com>
2016-04-15 02:37:39 +0800

    修正亞音速彈翻譯 datas/main.js 增加perttyPrint選項

--- datas/main.js
+++ datas/main.js
@@ -4,9 +4,14 @@ var packingDatasMain = require('./helper');
 var result = packingDatasMain(__dirname);
 
 var saveName = process.argv[2] || '';
+var prettyPrint = process.argv[3] || false;
+
 if (saveName) {
 	saveName = __dirname + '/' + saveName;
-	fs.writeFile(saveName, JSON.stringify(result, null, 4), function(err) {
+	var content = (prettyPrint)
+		? JSON.stringify(result, null, 4)
+		: JSON.stringify(result);
+	fs.writeFile(saveName, content, function(err) {
 		console.log('檔案已保存在路徑: ' + saveName);
 	});
 }
